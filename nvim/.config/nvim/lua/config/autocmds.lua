a7134a815234b593a06ebe471d6a90384303043d
meannzen <senmeann@gmail.com>
2026-07-07 08:53:19 +0700

    add back autocmd

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -10,7 +10,15 @@ autocmd("TextYankPost", {
 })
 
 -- Remove trailing whitespace on save
-
+autocmd("BufWritePre", {
+  group = augroup("trim_whitespace", { clear = true }),
+  pattern = "*",
+  callback = function()
+    local save = vim.fn.winsaveview()
+    vim.cmd([[%s/\s\+$//e]])
+    vim.fn.winrestview(save)
+  end,
+})
 -- Resize splits on window resize
 autocmd("VimResized", {
   group = augroup("resize_splits", { clear = true }),
--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,17 +1,17 @@
 return {
   {
-    "projekt0n/github-nvim-theme",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      require("github-theme").setup({
-        options = {
-          styles = {
-            comments = "italic",
-          },
-        },
-      })
-      vim.cmd("colorscheme github_dark_high_contrast")
-    end,
+    --"projekt0n/github-nvim-theme",
+    --lazy = false,
+   -- priority = 1000,
+    --config = function()
+     -- require("github-theme").setup({
+      --  options = {
+       --   styles = {
+         --   comments = "italic",
+        --  },
+       -- },
+      --})
+      --vim.cmd("colorscheme defaut")
+    --end,
   },
 }
