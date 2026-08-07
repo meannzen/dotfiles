c73c603b7825603e54863a68a1d1995e0b0cb04a
meannzen <senmeann@gmail.com>
2026-08-07 10:02:12 +0700

    change nvim

--- nvim/.config/nvim/lua/plugins/ui.lua
+++ nvim/.config/nvim/lua/plugins/ui.lua
@@ -1,14 +1,18 @@
 return {
+  -- Icons (used by lualine, bufferline, mini.files, telescope, alpha)
+  { "nvim-tree/nvim-web-devicons", lazy = true },
+
   -- Statusline
   {
     "nvim-lualine/lualine.nvim",
     event = "VeryLazy",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
     opts = {
       options = {
-        theme = "auto",
+        theme = "catppuccin-mocha",
         component_separators = { left = "|", right = "|" },
         section_separators = { left = "", right = "" },
-        icons_enabled = false,
+        icons_enabled = true,
         globalstatus = true,
       },
       sections = {
@@ -20,6 +24,56 @@ return {
         lualine_z = { "location" },
       },
     },
+  },
+
+  -- Buffer tabs
+  {
+    "akinsho/bufferline.nvim",
+    version = "*",
+    event = "VeryLazy",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    keys = {
+      { "<S-l>", "<cmd>BufferLineCycleNext<cr>", desc = "Next buffer" },
+      { "<S-h>", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev buffer" },
+      { "<leader>bp", "<cmd>BufferLineTogglePin<cr>", desc = "Pin buffer" },
+      { "<leader>bc", "<cmd>BufferLineCloseOthers<cr>", desc = "Close other buffers" },
+    },
+    opts = {
+      options = {
+        mode = "buffers",
+        separator_style = "thin",
+        diagnostics = "nvim_lsp",
+        always_show_bufferline = true,
+        show_close_icon = false,
+        show_buffer_close_icons = true,
+        offsets = {
+          { filetype = "minifiles", text = "File Explorer", highlight = "Directory", text_align = "left" },
+        },
+      },
+    },
+  },
+
+  -- Nicer notifications
+  {
+    "rcarriga/nvim-notify",
+    event = "VeryLazy",
+    opts = {
+      timeout = 3000,
+      render = "compact",
+      stages = "fade_in_slide_out",
+    },
+    config = function(_, opts)
+      local notify = require("notify")
+      notify.setup(opts)
+      vim.notify = notify
+    end,
+  },
+
+  -- Nicer vim.ui.select / vim.ui.input
+  {
+    "stevearc/dressing.nvim",
+    event = "VeryLazy",
+    opts = {},
   },
 
   -- Git signs in gutter
